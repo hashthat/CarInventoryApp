1a8d612a6f97a02f68c6f62d1249c2d53d8e881b
hashthat <samalberts73@gmail.com>
2025-04-25 13:25:48 -0500

    TestingFunctionality|FormattingData

---
+++ CarListApp/CarListApp/CarShow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CarListApp
+{
+    partial class CarShow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            vehicleDataGrid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)vehicleDataGrid).BeginInit();
+            SuspendLayout();
+            // 
+            // vehicleDataGrid
+            // 
+            vehicleDataGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            vehicleDataGrid.Location = new Point(3, 4);
+            vehicleDataGrid.Name = "vehicleDataGrid";
+            vehicleDataGrid.RowHeadersWidth = 51;
+            vehicleDataGrid.Size = new Size(785, 443);
+            vehicleDataGrid.TabIndex = 0;
+            vehicleDataGrid.CellContentClick += vehicleData_CellContentClick;
+            // 
+            // CarShow
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(vehicleDataGrid);
+            Name = "CarShow";
+            Text = "CarShow";
+            ((System.ComponentModel.ISupportInitialize)vehicleDataGrid).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView vehicleDataGrid;
+    }
+}
---
+++ CarListApp/CarListApp/CarShow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace CarListApp
+{
+    public partial class CarShow : Form
+    {
+        public CarShow()
+        {
+            InitializeComponent();
+        }
+
+        private void vehicleData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // connecting to the Database Developed through the tables for the VehicleList.mdf;  
+            string string2connect = @"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\C:\Users\earth\Desktop\CarInventoryApp\CarListApp\CarListApp\VehicleList.mdf;Integrated Security=True;Connect Timeout=45;";
+            string stringQuery = @"
+SELECT 'Car' AS Make, Model, Year, Price, Color, Transmission FROM [Table(car)]
+UNION ALL
+SELECT 'Truck', Make, Model, Year, Price, Color, TruckType, Drivetrain FROM [Table(Truck)]
+UNION ALL
+SELECT 'SUV', Make, Model, Year, Price, Color, Drivetrain, Condition FROM [Table(SUV)]";
+
+            SqlConnection conn = new SqlConnection(string2connect);
+            SqlDataAdapter adapter = new SqlDataAdapter(stringQuery, conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            vehicleDataGrid.DataSource = dt;
+
+
+        }
+    }
+}
